ff6206ae81786e0c2c9ac827a6ecd98ab8784ed9
RocoElWuero <usuario917@gmail.com>
2023-01-27 14:45:16 -0600

    Archivos para practicar el modulo 3 del archivo apuntes.txt.sql

---
+++ curso_datos.sql
@@ -0,0 +1,34 @@
+INSERT INTO cargo VALUES (1,'Empleado');
+INSERT INTO cargo VALUES (2,'Jefe de Departamento');
+INSERT INTO cargo VALUES (3,'Coordinador');
+INSERT INTO cargo VALUES (4,'Ejecutivo de cuenta');
+INSERT INTO cargo VALUES (5,'Subdirector');
+INSERT INTO cargo VALUES (6,'Director');
+
+INSERT INTO departamento VALUES (1,'Ventas');
+INSERT INTO departamento VALUES (2,'Recursos Humanos');
+INSERT INTO departamento VALUES (3,'Dise�o');
+INSERT INTO departamento VALUES (4,'Sistemas');
+INSERT INTO departamento VALUES (5,'AdministraciOn');
+INSERT INTO departamento VALUES (6,'Redes');
+
+INSERT INTO empleado VALUES (1,'Gilberto', 'Aparicio', 'Guerrero',27,'H',7000.00,NULL,'2001-03-20',4,1);
+INSERT INTO empleado VALUES (2,'Javier', 'Rojas', 'Hernandez',30,'H',7500.00,NULL,'1998-03-05',4,1);
+INSERT INTO empleado VALUES (3,'Alma', 'Guerra', 'Zarate',19,'M',10000.00,NULL,'2000-05-10',4,1);
+INSERT INTO empleado VALUES (4,'Carolina', 'Fernandez', 'Fernandez',32,'M',15000.00,NULL,'1989-12-01',4,2);
+INSERT INTO empleado VALUES (5,'Alberto', 'Ibanez', 'Becerril',32,'H',25000.00,NULL,'1990-05-16',4,2);
+INSERT INTO empleado VALUES (6,'Carlos', 'Lopez', 'Lopez',32,'H',27000.00,NULL,'1990-05-16',4,3);
+INSERT INTO empleado VALUES (7,'Arturo', 'Peña', 'Higareda',45,'H',18000.00,5,'1998-10-12',1,4);
+INSERT INTO empleado VALUES (8,'Javier', 'Barcenas', 'Moreno',29,'H',18000.00,10,'1994-03-20',1,4);
+INSERT INTO empleado VALUES (9,'Ernesto', 'Almaraz', 'Garcia',32,'H',20000.00,NULL,'1997-01-15',1,2);
+INSERT INTO empleado VALUES (10,'Susana', 'Garcia', 'Santos',31,'M',18000.00,NULL,'2002-11-30',3,1);
+INSERT INTO empleado VALUES (11,'Fernanda', 'Ramirez', 'Jimenez',18,'M',18000.00,NULL,'2002-08-30',3,1);
+INSERT INTO empleado VALUES (12,'Roxana', 'Guerrero', 'Vazquez',25,'M',26500.00,NULL,'1980-02-15',3,2);
+INSERT INTO empleado VALUES (13,'Manuel', 'Alavez', 'Nunez',35,'H',12600.00,NULL,'1989-04-30',2,2);
+INSERT INTO empleado VALUES (14,'Angeles', 'Nolasco', 'Carmona',29,'M',6000.00,NULL,'1997-07-15',5,1);
+INSERT INTO empleado VALUES (15,'Teresa', 'Alvarez', 'Martinez',37,'M',12000.00,NULL,'1999-10-15',5,2);
+INSERT INTO empleado VALUES (16,'Alberto', 'Gonzalez', 'Guizar',56,'H',35000.00,NULL,'1999-10-01',NULL,5);
+INSERT INTO empleado VALUES (17,'Ricardo', 'Perez', 'Rosas',58,'H',50500.00,NULL,'1999-10-01',NULL,6);
+INSERT INTO empleado VALUES (18,'Martha Elena', 'Ramirez', 'Torres',25,'M',26500.00,NULL,'1980-02-15',3,2);
+INSERT INTO empleado VALUES (19,'Elvira', 'Rios', 'Montes',50,'M',18000.00,7,'1994-03-20',1,4);
+
---
+++ curso_esquema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE cargo (
+  idCargo numeric(2,0) NOT NULL,
+  nombre varchar(255) NOT NULL,
+  CONSTRAINT pkCargo PRIMARY KEY (idCargo)
+)
+;
+
+CREATE TABLE departamento (
+  idDepartamento numeric(2,0) NOT NULL,
+  nombre varchar(100) NOT NULL,
+  CONSTRAINT pkDepartamento PRIMARY KEY  (idDepartamento)
+)
+;
+
+CREATE TABLE empleado (
+  idEmpleado numeric(4,0) NOT NULL,
+  nombre varchar(50) NOT NULL,
+  paterno varchar(50) NOT NULL,
+  materno varchar(50) NOT NULL,
+  edad numeric(2,0) NOT NULL,
+  sexo char(1) NOT NULL,
+  sueldo numeric(8,2) NOT NULL,
+  porcentaje_comision numeric(2,0),
+  contratacion date,
+  idDepartamento numeric(2,0),
+  idCargo numeric(2,0) NOT NULL,
+  CONSTRAINT pkEmpleado PRIMARY KEY  (idEmpleado)
+)
+;
